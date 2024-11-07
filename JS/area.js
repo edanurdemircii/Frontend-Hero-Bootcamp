14d1dc7453b7ccd8fefa59dabbe3ed468cb04ca5
edanurdemircii <ednrdmrc9@gmail.com>
2024-11-07 17:29:59 +0300

    restaurant cards added & button active javascript added

---
+++ JS/area.js
@@ -0,0 +1,12 @@
+fetch('areaData.json')
+.then(response => response.json())
+.then(data => {
+    const areaSelect = document.getElementById('areaSelect');
+    data.ilceler.forEach(ilce => {
+        const option = document.createElement('option');
+        option.value = ilce;
+        option.textContent = ilce;
+        areaSelect.appendChild(option);
+    });
+})
+.catch(error => console.error('Error loading the JSON:', error));
---
+++ JS/button.js
@@ -0,0 +1,5 @@
+const button = document.getElementById('select-button');
+
+button.addEventListener('click', function() {
+    button.classList.toggle('active');  
+});
